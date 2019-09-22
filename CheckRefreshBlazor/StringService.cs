a3de10005e07a7cee221ace792300c23a87679e8
pkaushik23 <pkaushik@donotdisclose.com>
2019-09-22 22:00:07 +0530

    new blazor app

---
+++ CheckRefreshBlazor/StringService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheckRefreshBlazor
+{
+    public class StringService
+    {
+        public List<string> Names { get; set; } = new List<string>();
+       public void Add()
+        {
+            this.Names.Add("another name");
+        }
+    }
+}
